36f837e9dd11f738b8ff97f81b12cda2615dee36
rotem <rotemd884@gmail.com>
2024-11-14 19:13:22 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+	    String inputWord = args[0];
+            String word = inputWord.toUpperCase();
+            int times = Integer.parseInt(args[1]);
+            String sCheck = "AEFHILMNORSX";
+            char letter;
+            for (int i = 0; i < word.length(); i++) {
+                letter = word.charAt(i);
+                if (sCheck.indexOf(letter) == -1 ) {
+                        System.out.println("Give me a  " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                }
+                else
+                {
+                        System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                }
+            }
+
+            System.out.println("What does that spell?");
+
+            for (int i = 0; i < times; i++) {
+                System.out.println(word + "!!!");
+            }
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,32 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		boolean bool = args[1].equals("v");
+		int i;
+		int counter = 2;
+		for (int j = 1; j <= seed; j++) {
+			i = j;
+			if (i == 1) {
+				i = 4;
+				if(bool) {System.out.print("1 ");}
+			}
+			while (i != 1) {
+				if(bool) {System.out.print(i + " ");}
+				if (i % 2 == 0) {
+					i = i / 2;
+				}
+				else
+				{
+					i = i * 3 + 1;
+				}
+				counter++;
+			}
+			if(bool) {System.out.print("1 (" + counter + ")\n");}
+			counter = 1;
+		}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+
+
 	}
 }
